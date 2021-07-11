149d35b7e580794133c5c3bb9b024dd2197fd298
Victoriia Murakhovskaya <inbox@victoriia.online>
2021-07-11 14:50:53 +0300

    Code cleaning, doc strings.

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ import sqlite3
 from .utils import read_db_path
 import pathlib
 import logging
-import pandas as pd
 import numpy as np
 
 
@@ -30,8 +29,7 @@ class YFinance:
     def __init__(self):
 
         # init logger
-        logger_path = pathlib.Path(
-            __file__).parent.resolve() / f'logs/log_db_{datetime.now().date()}_{datetime.now().time().hour}_{datetime.now().time().minute}.log'
+        logger_path = pathlib.Path(__file__).parent.resolve() / f'logs/log_db_{datetime.now().date()}_{datetime.now().time().hour}_{datetime.now().time().minute}.log'
         self.logger = logging.getLogger('db_logger')
         _log_format = f"[%(levelname)s] - %(asctime)s - %(message)s"
         self.logger.setLevel(logging.INFO)
@@ -98,6 +96,7 @@ class YFinance:
         """
         Filter tickers list considering actual tickers list
         :param tickers_list: tickers list to filter
+        :param flag: True - adding tickers, False - deleting tickers
         :return: filtered tickers list
         """
         # get current list of tickers and make a filtered list to insert
@@ -163,71 +162,127 @@ class YFinance:
         """
         return self.proceed_tickers(tickers_list, False)
 
-    def del_all_tickers(self):
-        cur = self.conn.cursor()
-        cur.execute(self.DELETE_ALL_TICKERS)
-        self.conn.commit()
-        self._tickers = self.read_tickers_from_db()
+    def del_all_tickers(self) -> bool:
+        """
+        Delete all tickers from the DB
+        :return:
+        """
+        try:
+            cur = self.conn.cursor()
+            cur.execute(self.DELETE_ALL_TICKERS)
+            self.conn.commit()
+            self._tickers = self.read_tickers_from_db()
+            self.logger.info('All tickers are deleted from the DB')
+            return True
+        except Exception as ex:
+            self.logger.error(f'An error while deleting all tickers. {ex}')
 
     ####
     # PROCEED DATA
     ####
 
-    def initial_data_load(self, load_for_tickers=None):
-
+    def initial_data_load(self, load_for_tickers=None) -> bool:
+        """
+        Load data for a long period to the DB
+        :param load_for_tickers: The set of tickers (if different from self._tickers)
+        :return:
+        """
         if load_for_tickers is None:
             load_for_tickers = self._tickers
 
         # reading data
-        finish_date = datetime.now().date()
-        start_date = (datetime.now() - timedelta(days=self.INITIAL_DEPTH)).date()
-       # data = yf.download(load_for_tickers, start=start_date, end=finish_date)
-        data = yf.download("AAPL", prepost='1m')
-        data = data.loc[:, data.columns.get_level_values(0) == 'Close'].copy()
-        data.columns = data.columns.droplevel()
+        try:
+            finish_date = datetime.now().date()
+            start_date = (datetime.now() - timedelta(days=self.INITIAL_DEPTH)).date()
+            data = yf.download(load_for_tickers, start=start_date, end=finish_date)
+            data = data.loc[:, data.columns.get_level_values(0) == 'Close'].copy()
+            data.columns = data.columns.droplevel()
 
-        # database insertion
-        records = []
-        for data_date in list(data.index):
-            for ticker in self._tickers.split(' '):
-                if not np.isnan(data.at[data_date, ticker]):
-                    records.append((data_date.date(), ticker, data.at[data_date, ticker]))
+            # database insertion
+            records = []
+            for data_date in list(data.index):
+                for ticker in self._tickers.split(' '):
+                    if not np.isnan(data.at[data_date, ticker]):
+                        records.append((data_date.date(), ticker, data.at[data_date, ticker]))
+        except Exception as ex:
+            self.logger.error(f'Error while loading data using YFinance. {ex}')
+            return False
 
         self.save_data_to_db(records)
+        return True
 
     def save_data_to_db(self, data_list):
-        cur = self.conn.cursor()
-        cur.executemany(self.WRITE_DATA, data_list)
-
-        logging.info(f'{len(data_list)} rows inserted in db')
-
-        self.conn.commit()
-
-    def get_db_tickers(self):
-        cur = self.conn.cursor()
-        cur.execute(self.READ_TICKERS)
-        rows = cur.fetchall()
-        return [row[0] for row in rows]
-
-    def update_db(self):
-
-        # first case - more tickers in _tickers than in db, less tickers in _tickers than in db
-        tickers_list = self._tickers.split(' ')
-        db_tickers = self.get_db_tickers()
-        list_for_update = [item for item in tickers_list if item not in db_tickers]
-        list_for_delete = [item for item in db_tickers if item not in tickers_list]
-
-        if list_for_update:
-            self.initial_data_load(load_for_tickers=list_for_update)
-
-        if list_for_delete:
+        """
+        Saves fetched data to the DB
+        :param data_list: Data to save (list of tuples)
+        :return:
+        """
+        try:
             cur = self.conn.cursor()
-            deletion_data = [(item, ) for item in list_for_delete]
-            cur.executemany(self.DELETE_SELECTION, deletion_data)
+            cur.executemany(self.WRITE_DATA, data_list)
 
-    def del_all_from_db(self):
-        cur = self.conn.cursor()
-        cur.execute(self.DELETE_ALL)
-        self.conn.commit()
+            logging.info(f'{len(data_list)} rows inserted in db')
 
+            self.conn.commit()
+            return True
 
+        except Exception as ex:
+            self.logger.error(f'Error while writing to the DB. {ex}')
+            return False
+
+    def get_db_tickers(self) -> list:
+        """
+        Reads tickers from the DB
+        :return: A list of tickers
+        """
+        try:
+            cur = self.conn.cursor()
+            cur.execute(self.READ_TICKERS)
+            rows = cur.fetchall()
+            self.logger.info('Tickers are read')
+            return [row[0] for row in rows]
+        except Exception as ex:
+            self.logger.error(f'Error reading tickers from the DB. {ex}')
+            return []
+
+    def update_db(self) -> bool:
+        """
+        Updates information in the DB
+        :return: boolean result
+        """
+        # first case - more tickers in _tickers than in db, less tickers in _tickers than in db
+        try:
+            tickers_list = self._tickers.split(' ')
+            db_tickers = self.get_db_tickers()
+            list_for_update = [item for item in tickers_list if item not in db_tickers]
+            list_for_delete = [item for item in db_tickers if item not in tickers_list]
+
+            if list_for_update:
+                self.initial_data_load(load_for_tickers=list_for_update)
+
+            if list_for_delete:
+                cur = self.conn.cursor()
+                deletion_data = [(item, ) for item in list_for_delete]
+                cur.executemany(self.DELETE_SELECTION, deletion_data)
+
+            self.conn.commit()
+            self.logger.info(f'The DB updated successfully')
+            return True
+        except Exception as ex:
+            self.logger.error(f'Error updating the database. {ex}')
+            return False
+
+    def del_all_from_db(self) -> bool:
+        """
+        Clears the database.
+        :return: result (boolean)
+        """
+        try:
+            cur = self.conn.cursor()
+            cur.execute(self.DELETE_ALL)
+            self.conn.commit()
+            self.logger.info('The database is cleaned')
+            return True
+        except Exception as ex:
+            self.logger.error(f'Error while cleaning the DB. {ex}')
+            return False
